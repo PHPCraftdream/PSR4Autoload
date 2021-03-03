76937b0b734523578d139da224fcd33d98258b70
Marat K <klyde13@gmail.com>
2021-03-03 13:52:18 +0300

    fix php open tag; fix version; fix tabs to spaces

--- README.md
+++ README.md
@@ -3,34 +3,29 @@
 
 ## Installation
 
-Add the following to your composer.json and run `composer update`
+Run
 
-```json
-{
-	"require":
-	{
-		"phpcraftdream/psr4utoload": "dev-master"
-	}
-}
+```
+composer require phpcraftdream/psr4utoload
 ```
 
 ## Usage
 
 ```php
 
-	require_once './vendor/autoload.php';
+    require_once './vendor/autoload.php';
 
-	$psr4utoload = new \PHPCraftdream\PSR4Autoload\PSR4Autoload();
+    $psr4utoload = new \PHPCraftdream\PSR4Autoload\PSR4Autoload();
 
-	$psr4utoload->register();
+    $psr4utoload->register();
 
-	$psr4utoload->setPaths(
-		[
-			'NameSpace1' => '/dir1/',
-			'NameSpace2' => '/dir2/',
-			'NameSpace3' => '/dir3/',
-		]
-	);
+    $psr4utoload->setPaths(
+        [
+            'NameSpace1' => '/dir1/',
+            'NameSpace2' => '/dir2/',
+            'NameSpace3' => '/dir3/',
+        ]
+    );
 
-	$obj = new \NameSpace1\className();
+    $obj = new \NameSpace1\className();
 ```
--- src/PSR4Autoload.php
+++ src/PSR4Autoload.php
@@ -1,62 +1,62 @@
-<?
-	namespace PHPCraftdream\PSR4Autoload;
+<?php
+    namespace PHPCraftdream\PSR4Autoload;
 
-	class PSR4Autoload
-	{
-		public $paths = [];
+    class PSR4Autoload
+    {
+        public $paths = [];
 
-		public function makeDirPath($dir)
-		{
-			$ds = DIRECTORY_SEPARATOR;
+        public function makeDirPath($dir)
+        {
+            $ds = DIRECTORY_SEPARATOR;
 
-			$dir = str_replace(['\\', '/'], $ds, $dir);
-			$dir = realpath($dir);
-			$dir = rtrim($dir, $ds) . $ds;
+            $dir = str_replace(['\\', '/'], $ds, $dir);
+            $dir = realpath($dir);
+            $dir = rtrim($dir, $ds) . $ds;
 
-			return $dir;
-		}
+            return $dir;
+        }
 
-		public function setPaths(array $paths)
-		{
-			foreach ($paths as $namespace => $path)
-				$this->paths[$namespace] = $this->makeDirPath($path);
-		}
+        public function setPaths(array $paths)
+        {
+            foreach ($paths as $namespace => $path)
+                $this->paths[$namespace] = $this->makeDirPath($path);
+        }
 
-		public function register()
-		{
-			spl_autoload_register([$this, 'loadClass']);
-		}
+        public function register()
+        {
+            spl_autoload_register([$this, 'loadClass']);
+        }
 
-		public function loadClass($class)
-		{
-			foreach ($this->paths as $namespace => $path)
-			{
-				$found = $this->loadClassByNsAndPath($class, $namespace, $path);
-				if (!$found) continue;
+        public function loadClass($class)
+        {
+            foreach ($this->paths as $namespace => $path)
+            {
+                $found = $this->loadClassByNsAndPath($class, $namespace, $path);
+                if (!$found) continue;
 
-				return;
-			}
-		}
+                return;
+            }
+        }
 
-		public function loadClassByNsAndPath($class, $namespace, $path)
-		{
-			$ds = DIRECTORY_SEPARATOR;
+        public function loadClassByNsAndPath($class, $namespace, $path)
+        {
+            $ds = DIRECTORY_SEPARATOR;
 
-			$len = strlen($namespace);
+            $len = strlen($namespace);
 
-			if (substr($class, 0, $len) !== $namespace)
-				return false;
+            if (substr($class, 0, $len) !== $namespace)
+                return false;
 
-			$className = substr($class, 1 + $len);
-			$className = str_replace(['/', '\\'], $ds, $className);
+            $className = substr($class, 1 + $len);
+            $className = str_replace(['/', '\\'], $ds, $className);
 
-			$path = $path . $className . '.php';
+            $path = $path . $className . '.php';
 
-			if (!is_file($path))
-				return false;
+            if (!is_file($path))
+                return false;
 
-			require_once $path;
+            require_once $path;
 
-			return true;
-		}
-	}
+            return true;
+        }
+    }
